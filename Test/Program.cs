ed84339651e2655619a5584a4eb6490ceff6629c
Shingo <geniusz2009-zxy@yahoo.com.cn>
2021-08-08 20:52:42 +0800

    upgrade with upgrade-assistant add Test project

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			int i = int.MaxValue, j;
+			var k = checked(j = Interlocked.Increment(ref i));
+			Console.WriteLine(k);
+			Console.WriteLine(j);
+		}
+	}
+}
